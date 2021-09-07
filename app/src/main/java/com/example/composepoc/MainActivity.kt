c5ad430b833ab9a5dc0f2f4ed3743ab98b3b38d5
Akhil Jain <akhilj33@gmail.com>
2021-09-07 16:08:22 +0530

    work

--- app/src/main/java/com/example/composepoc/MainActivity.kt
+++ app/src/main/java/com/example/composepoc/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.composepoc
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.gestures.rememberTransformableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
@@ -18,6 +21,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
@@ -25,12 +29,16 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.layout.positionInParent
+import androidx.compose.ui.layout.positionInRoot
+import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.composepoc.ui.theme.ComposePOCTheme
 import kotlin.math.roundToInt
 
@@ -48,7 +56,8 @@ fun SetMainScreen(userHandle: String) {
     val offsetX = remember { mutableStateOf(200f) }
     val offsetY = remember { mutableStateOf(100f) }
     val size = remember { mutableStateOf(Size.Zero) }
-    val height = remember { mutableStateOf(0) }
+    val height = remember { mutableStateOf(0f) }
+    val width = remember { mutableStateOf(0f) }
     val scale = remember { mutableStateOf(1f) }
 
     Box(modifier = Modifier
@@ -57,10 +66,13 @@ fun SetMainScreen(userHandle: String) {
         .onGloballyPositioned {
             size.value = it.size.toSize()
         }) {
-        CommentBox(userHandle, offsetX, offsetY, size, height, scale)
+        CommentBox(userHandle, offsetX, offsetY, size, height, width, scale)
     }
 
 }
+
+val maxScale = 4f
+val minScale = 0.7f
 
 @Composable
 fun CommentBox(
@@ -68,30 +80,77 @@ fun CommentBox(
     offsetX: MutableState<Float>,
     offsetY: MutableState<Float>,
     size: MutableState<Size>,
-    height: MutableState<Int>,
+    height: MutableState<Float>,
+    width: MutableState<Float>,
     scale: MutableState<Float>
 ) {
+    fun calculateNewScale(k: Float): Float =
+        if ((scale.value <= maxScale && k > 1f) || (scale.value >= minScale && k < 1f)) scale.value * k else scale.value
+
+//    val state = rememberTransformableState { zoomChange, offsetChange, rotationChange ->
+//        scale *= zoomChange
+//        rotation += rotationChange
+//        offset += offsetChange
+//    }
+
     Surface(color = Color.Black, shape = RoundedCornerShape(8.dp),
         modifier = Modifier
             .offset { IntOffset(offsetX.value.roundToInt(), offsetY.value.roundToInt()) }
             .width(200.dp)
             .onSizeChanged {
-                height.value = it.height
+                width.value = it.width.toFloat()
+                height.value = it.height.toFloat()
             }
-//            .onGloballyPositioned {
-//                height.value = it.size.height
-//            }
+
             .pointerInput(Unit) {
                 detectTransformGestures { centroid, dragAmount, zoom, rot ->
-                    val original = Offset(offsetX.value, offsetY.value)
+                    var original = Offset(offsetX.value, offsetY.value)
+
+                    val newScale = calculateNewScale(zoom)
+
+
+                    if (newScale != scale.value && (size.value.width - width.value * newScale) >= 0f &&
+                        (size.value.height - height.value * newScale) >= 0f
+                    ) {
+//                        Log.d("Akhil", "newscale ${original.x - ((width.value * newScale - width.value) / 2)}")
+//                        original -= Offset(((width.value * newScale - width.value) / 2), ((height.value * newScale - height.value) / 2))
+//                         original = Offset(
+//                            original.x - ((width.value * newScale - width.value) / 2),
+//                            original.y - ((height.value * newScale - height.value) / 2)
+//                        )
+                        Log.d("Akhil", "original x ${original.x - ((width.value * newScale - width.value) / 2)}")
+                        Log.d("Akhil", "drag ${original.x - ((width.value * newScale - width.value) / 2) + dragAmount.x}")
+
+                        width.value = width.value * newScale
+                        height.value = height.value * newScale
+                        scale.value = newScale
+//                        original = Offset((centroid.x - (width.value / 2)), (centroid.y - (height.value / 2)))
+                    }
+
+//                    Log.d("Akhil", "total width ${size.value.width} width ${width.value}, height ${height.value}")
+
+//
+//
+//                    if ((size.value.height - height.value * newScale) >= 0f)
+//                        height.value = height.value * newScale
+//
+//                    val x = size.value.width - width.value
+//                    val y = size.value.height - height.value
+//                    if (x >= 0f && y >= 0f) scale.value = newScale
+//                    Log.d("Akhil", "dragAmount x $dragAmount")
                     val summed = original + dragAmount
-                    val newValue = Offset(
-                        x = summed.x.coerceIn(0f, size.value.width - 200.dp.toPx()),
-                        y = summed.y.coerceIn(0f, size.value.height - height.value.toFloat())
+//                    Log.d("Akhil", "summed x ${summed.x}")
+                    val newValueAfterDrag = Offset(
+                        x = summed.x.coerceIn(0f, size.value.width - width.value),
+                        y = summed.y.coerceIn(0f, size.value.height - height.value)
                     )
-                    offsetX.value = newValue.x
-                    offsetY.value = newValue.y
-                    scale.value *= zoom
+
+//                    Log.d("Akhil", "x ${newValueAfterDrag.x}, y ${newValueAfterDrag.y}")
+
+                    offsetX.value = newValueAfterDrag.x
+                    offsetY.value = newValueAfterDrag.y
+//                    scale.value *= zoom
+
 
                 }
             }
@@ -119,3 +178,58 @@ fun DefaultPreview() {
         SetMainScreen("@akj33 \n cgjdhcbdhbcdjshbc")
     }
 }
+
+
+//@Composable
+//fun ZoomableImage(imageUrl: String?, maxScale: Float = 4f, minScale: Float = 0.7f) {
+//    val scale by remember { mutableStateOf(1f) }
+//    val bitmap = remember { mutableStateOf<Bitmap?>(null) }
+//    val translation by remember { mutableStateOf(Offset(0f, 0f)) }
+//    fun calculateNewScale(k: Float): Float =
+//        if ((scale <= maxScale && k > 1f) || (scale >= minScale && k < 1f)) scale * k else scale
+//    Glide.with(AmbientContext.current).asBitmap()
+//        .load(imageUrl)
+//        .into(object : CustomTarget<Bitmap>() {
+//            override fun onLoadCleared(placeholder: Drawable?) {}
+//            override fun onResourceReady(
+//
+//                resource: Bitmap,
+//                transition: com.bumptech.glide.request.transition.Transition<in Bitmap>?
+//            ) {
+//                bitmap.value = resource
+//            }
+//        })
+//    Column(
+//        verticalArrangement = Arrangement.Center,
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        modifier = Modifier
+//            .background(Color.Black)
+//
+//    ) {
+//        bitmap?.let { loadedBitmap ->
+//            Image(
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .zoomable(onZoomDelta = {
+//                        scale = calculateNewScale(it)
+//                    })
+//                    .rawDragGestureFilter(
+//                        object : DragObserver {
+//                            override fun onDrag(dragDistance: Offset): Offset {
+//                                translation = translation.plus(dragDistance)
+//                                return super.onDrag(dragDistance)
+//                            }
+//                        })
+//                    .graphicsLayer(
+//                        scaleX = scale,
+//                        scaleY = scale,
+//                        translationX = translation.x,
+//                        translationY = translation.y
+//                    ),
+//                bitmap = loadedBitmap.asImageBitmap()
+//            )
+//        } ?: kotlin.run {
+//            Text("Loading Image...", color = Color.White)
+//        }
+//    }
+//}
---
+++ app/src/main/java/com/example/composepoc/Zoomable.kt
@@ -0,0 +1,203 @@
+package com.example.composepoc
+
+import androidx.compose.foundation.gestures.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.center
+import androidx.compose.ui.input.pointer.*
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.toSize
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
+
+/**
+ * A zoomable layout that supports zooming in and out, dragging, double tap and dismiss gesture.
+ *
+ * @param modifier The modifier to apply to this layout.
+ * @param state The state object to be used to control or observe the state.
+ * @param enabled Controls the enabled state. When false, all gestures will be ignored.
+ * @param dismissGestureEnabled Whether to enable dismiss gesture detection.
+ * @param onDismiss Will be called when dismiss gesture is detected. Should return a boolean
+ * indicating whether the dismiss request is handled.
+ * @param content a block which describes the content.
+ */
+
+@Composable
+fun Zoomable(
+    modifier: Modifier = Modifier,
+    state: ZoomableState = rememberZoomableState(),
+    enabled: Boolean = true,
+    dismissGestureEnabled: Boolean = false,
+    onDismiss: () -> Boolean = { false },
+    content: @Composable () -> Unit
+) {
+    val dismissGestureEnabledState = rememberUpdatedState(dismissGestureEnabled)
+    val scope = rememberCoroutineScope()
+    val gesturesModifier = if (!enabled) Modifier else Modifier
+        .pointerInput(Unit) {
+            detectTapAndDragGestures(
+                state = state,
+                dismissGestureEnabled = dismissGestureEnabledState,
+                onDismiss = onDismiss
+            )
+        }
+        .transformable(state = rememberTransformableState { zoomChange, panChange, _ ->
+            if (state.dismissDragAbsoluteOffsetY == 0f) {
+                scope.launch {
+                    state.onZoomChange(zoomChange)
+                    state.onDrag(panChange)
+                }
+            }
+        })
+
+    Box(
+        modifier = modifier
+            .then(gesturesModifier)
+            .layout { measurable, constraints ->
+                val width = constraints.maxWidth
+                val height = constraints.maxHeight
+                val placeable = measurable.measure(
+                    Constraints(
+                        maxWidth = (width * state.scale).roundToInt(),
+                        maxHeight = (height * state.scale).roundToInt()
+                    )
+                )
+                state.size = IntSize(width, height)
+                state.childSize = Size(
+                    placeable.width / state.scale,
+                    placeable.height / state.scale
+                )
+                layout(width, height) {
+                    placeable.place(
+                        state.translationX.roundToInt() - state.boundOffset.x,
+                        state.translationY.roundToInt() - state.boundOffset.y
+                                + state.dismissDragOffsetY.roundToInt()
+                    )
+                }
+            }
+    ) {
+        content()
+    }
+}
+
+internal suspend fun PointerInputScope.detectTapAndDragGestures(
+    state: ZoomableState,
+    dismissGestureEnabled: State<Boolean>,
+    onDismiss: () -> Boolean
+) = coroutineScope {
+    launch {
+        detectTapGestures(
+            onDoubleTap = { offset ->
+                launch {
+                    val isZooming = state.isZooming
+                    val targetScale =
+                        if (isZooming) state.minScale else state.doubleTapScale
+                    state.animateScaleTo(
+                        targetScale = targetScale,
+                        targetTranslation = if (isZooming) {
+                            Offset.Zero
+                        } else {
+                            state.calculateTargetTranslation(offset) * targetScale
+                        }
+                    )
+                }
+            },
+            onPress = {
+                state.onPress()
+            }
+        )
+    }
+    launch {
+        detectDragGestures(
+            state = state,
+            dismissGestureEnabled = dismissGestureEnabled,
+            onDrag = { change, dragAmount ->
+                if (state.isZooming) {
+                    launch {
+                        state.onDrag(dragAmount)
+                        state.addPosition(
+                            change.uptimeMillis,
+                            change.position
+                        )
+                    }
+                } else {
+                    state.onDismissDrag(dragAmount.y)
+                }
+            },
+            onDragCancel = {
+                if (state.isZooming) {
+                    state.resetTracking()
+                } else {
+                    launch {
+                        state.onDismissDragEnd()
+                    }
+                }
+            },
+            onDragEnd = {
+                launch {
+                    if (state.isZooming) {
+                        state.onDragEnd()
+                    } else {
+                        if (!(state.shouldDismiss && onDismiss())) {
+                            state.onDismissDragEnd()
+                        }
+                    }
+                }
+            }
+        )
+    }
+}
+
+private suspend fun PointerInputScope.detectDragGestures(
+    state: ZoomableState,
+    dismissGestureEnabled: State<Boolean>,
+    onDragEnd: () -> Unit = {},
+    onDragCancel: () -> Unit = {},
+    onDrag: (change: PointerInputChange, dragAmount: Offset) -> Unit
+) {
+    forEachGesture {
+        awaitPointerEventScope {
+            // We have to always call this or we'll get a crash if we do nothing
+            val down = awaitFirstDown(requireUnconsumed = false)
+            if (state.isZooming || dismissGestureEnabled.value) {
+                var overSlop = Offset.Zero
+                val drag = if (state.isZooming) {
+                    awaitTouchSlopOrCancellation(down.id) { change, over ->
+                        change.consumePositionChange()
+                        overSlop = over
+                    }
+                } else {
+                    awaitVerticalTouchSlopOrCancellation(down.id) { change, over ->
+                        change.consumePositionChange()
+                        overSlop = Offset(0f, over)
+                    }
+                }
+                if (drag != null) {
+                    onDrag(drag, overSlop)
+                    if (
+                        !drag(drag.id) {
+                            onDrag(it, it.positionChange())
+                            it.consumePositionChange()
+                        }
+                    ) {
+                        onDragCancel()
+                    } else {
+                        onDragEnd()
+                    }
+                }
+            }
+        }
+    }
+}
+
+private fun ZoomableState.calculateTargetTranslation(doubleTapPoint: Offset): Offset =
+    (size.toSize().center + Offset(translationX, translationY) - doubleTapPoint) / scale
